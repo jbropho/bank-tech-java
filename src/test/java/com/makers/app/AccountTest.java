d370e593f3407bae1104856317562372b2ff0513
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-18 14:47:35 +0100

    add deposit method

--- src/test/java/com/makers/app/AccountTest.java
+++ src/test/java/com/makers/app/AccountTest.java
@@ -13,4 +13,12 @@ public class AccountTest
       Account testAccount = new Account(0);
       assertEquals(testAccount.getBalance(), expected);
     }
+
+    public void testDeposit()
+    {
+      int expected = 10;
+      Account testAccount = new Account(0);
+      testAccount.deposit(10);
+      assertEquals(testAccount.getBalance(), expected);
+    }
 }
