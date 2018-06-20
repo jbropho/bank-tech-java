a6fca4901abbf638e3538cebd39d3576941ad0b5
Jordan Brooks <jorbrooks92@gmail.com>
2018-06-20 17:18:36 +0100

    update to reflect earlier formatting

--- src/main/java/com/makers/app/Account.java
+++ src/main/java/com/makers/app/Account.java
@@ -21,11 +21,11 @@ public class Account {
 
   public void deposit(int amount) {
     this.balance += amount;
-    statement.addTransaction(Integer.toString(amount), "deposit");
+    statement.addTransaction(amount, "deposit");
   }
 
   public void withdraw(int amount) {
     this.balance -= amount;
-    statement.addTransaction(Integer.toString(amount), "withdraw");
+    statement.addTransaction(amount, "withdraw");
   }
 }
